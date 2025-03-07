79c7c8729230bff5d22951334b4e7921eef805f0
dikcoding <andikafadillasiagian0@gmail.com>
2025-03-07 14:44:43 +0700

    Learn Matplotlib

--- 8_Matplotlib_Kelas Terbuka/07-Legend/main.py
+++ 8_Matplotlib_Kelas Terbuka/07-Legend/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Membuat Data
+# Membuat Data	
 def sinusGenerator(amplitudo,frekuensi,tAkhir,theta):
 	t = np.arange(0,tAkhir,0.1)
 	y = amplitudo * np.sin(2*frekuensi*t + np.deg2rad(theta))
